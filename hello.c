6f4f1c91844eebc61c4a9cc44cfa234e2d5d7743
kaviya <38944278+kaviyarajendrian@users.noreply.github.com>
2019-03-06 21:59:49 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+   
+   int n,i;
+   printf("Enter the number:");
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+       printf("\n HELLO");
+   }
+    return 0;
+}
