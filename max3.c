647f2d83926a44c2b3cc448c916b2fe1cb036d9f
kaviya <38944278+kaviyarajendrian@users.noreply.github.com>
2019-03-15 21:38:20 +0530

    Create max3.c

---
+++ max3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int array[10],size,i,largest;
+    printf("\n Enter the size of the array:");
+    scanf("%d",&size);
+    printf("\n Enter %d elements of the array:",size);
+    for(i=0;i<size;i++)
+    scanf("%d",&array[i]);
+    largest=array[0];
+    for(i=0;i<size;i++)
+    {
+        if(largest<array[i])
+        largest=array[i];
+    }
+    printf("\n largest element present in the given array is:%d",largest);
+    return 0;
+}
