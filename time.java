f4b288366dfa96875ac7f28288e56955daadc027
kaviya <38944278+kaviyarajendrian@users.noreply.github.com>
2019-08-17 16:20:18 +0530

    Create time.java

---
+++ time.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Time{
+    public static void main(String args[]){
+        double runtime = 52;
+        double hours = 52/60;
+        System.out.println(""+hours+""+runtime);
+    }
+   }
